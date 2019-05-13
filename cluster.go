15506e982eafa484b43144e3bb5d9076e4b8aa9e
Evan Digby <evandigby@gmail.com>
2019-05-13 10:51:34 -0700

    adding cluster

--- cluster.go
+++ cluster.go
@@ -2,31 +2,39 @@ package kafka
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
 
 	"github.com/evandigby/kafka/api/metadata"
 )
 
+// ErrLeaderNotAvailable is returned when the leader for a topic/partition is not available
+var ErrLeaderNotAvailable = errors.New("leader not available")
+
 // Cluster represents a Kafka cluster
 type Cluster struct {
 	clientID string
 	sasl     SASLConfig
 
+	bootstrapServers []string
+
+	lastMeta                 metadata.Response
 	brokers                  map[int32]*Broker
 	brokersMux               sync.RWMutex
-	topicPartitionLeaders    map[int32]map[string]*Broker
+	topicPartitionLeaders    map[string]map[int32]*Broker
 	topicPartitionLeadersMux sync.RWMutex
 }
 
 // NewCluster creates a new Kafka cluster
 func NewCluster(bootstrap []string, config ClusterConfig) (*Cluster, error) {
 	c := &Cluster{
-		clientID: config.ClientID,
-		sasl:     config.SASL,
+		bootstrapServers: bootstrap,
+		clientID:         config.ClientID,
+		sasl:             config.SASL,
 	}
 
-	err := c.bootstrap(bootstrap)
+	err := c.bootstrap()
 	if err != nil {
 		return nil, err
 	}
@@ -34,9 +42,9 @@ func NewCluster(bootstrap []string, config ClusterConfig) (*Cluster, error) {
 	return c, nil
 }
 
-func (c *Cluster) bootstrap(servers []string) error {
+func (c *Cluster) bootstrap() error {
 	for {
-		for _, s := range servers {
+		for _, s := range c.bootstrapServers {
 			var (
 				mresp metadata.Response
 				merr  error
@@ -77,23 +85,116 @@ func (c *Cluster) bootstrap(servers []string) error {
 				continue
 			}
 
-			c.updateBrokers(mresp)
+			err = c.updateBrokers(mresp, brokerConfig)
+			if err != nil {
+				fmt.Println("error updating brokers:", err)
+				continue
+			}
+
+			err = c.updateTopicPartitionLeaders(mresp)
+			if err != nil {
+				fmt.Println("error updating topic partition leaders:", err)
+				continue
+			}
+
+			return nil
 		}
 	}
 }
 
-func (c *Cluster) updateBrokers(meta metadata.Response) {
+func (c *Cluster) updateBrokers(meta metadata.Response, config BrokerConfig) error {
+	type brokerConnect struct {
+		nodeID int32
+		broker *Broker
+		err    error
+	}
+
+	brokerChan := make(chan brokerConnect)
+
+	var wg sync.WaitGroup
+	wg.Add(len(meta.Brokers()))
+	go func() {
+		defer close(brokerChan)
+		wg.Wait()
+	}()
+
+	for _, b := range meta.Brokers() {
+		go func(id int32, host string) {
+			defer wg.Done()
+			broker, err := NewBroker(host, config)
+			brokerChan <- brokerConnect{
+				broker: broker,
+				err:    err,
+				nodeID: id,
+			}
+		}(b.NodeID(), fmt.Sprintf("%v:%v", b.Host(), b.Port()))
+	}
+
+	var errs []error
+	brokers := map[int32]*Broker{}
+	for b := range brokerChan {
+		if b.err != nil {
+			errs = append(errs, b.err)
+			continue
+		}
+
+		brokers[b.nodeID] = b.broker
+	}
+
+	if len(errs) > 0 {
+		return errs[0] // TODO: find a better thing to return
+	}
+
+	c.brokersMux.Lock()
+	defer c.brokersMux.Unlock()
+	oldBrokers := c.brokers
+	c.brokers = brokers
+
+	if oldBrokers != nil {
+		go func() {
+			for _, b := range oldBrokers {
+				b.Close()
+			}
+		}()
+	}
+	return nil
+}
+
+func (c *Cluster) updateTopicPartitionLeaders(meta metadata.Response) error {
+	topicPartitionLeaders := map[string]map[int32]*Broker{}
+
+	c.topicPartitionLeadersMux.Lock()
+	defer c.topicPartitionLeadersMux.Unlock()
+
+	c.brokersMux.RLock()
+	defer c.brokersMux.RUnlock()
+	for _, t := range meta.Topics() {
+		topicInfo := map[int32]*Broker{}
+		for _, p := range t.Partitions() {
+			leader, ok := c.brokers[p.Leader()]
+			if !ok {
+				return fmt.Errorf("topic %v, partition %v: %w", t.Topic(), p.Partition(), ErrLeaderNotAvailable)
+			}
+			topicInfo[p.Partition()] = leader
+		}
+
+		topicPartitionLeaders[t.Topic()] = topicInfo
+	}
+	return nil
+}
+
+// Close closes the connection to all connected brokers
+func (c *Cluster) Close() {
 	c.brokersMux.Lock()
 	defer c.brokersMux.Unlock()
 
-	brokers := map[int32]*Broker{}
-	for _, b := range meta.Brokers() {
-		var err error
-		brokers[b.NodeID()], err = NewBroker(fmt.Sprintf("%v:%v", b.Host(), b.Port()), c BrokerConfig)
+	var wg sync.WaitGroup
+	wg.Add(len(c.brokers))
+	for _, b := range c.brokers {
+		go func(b *Broker) {
+			defer wg.Done()
+			b.Close()
+		}(b)
 	}
-}
-
-func (c *Cluster) updateTopicPartitionLeaders(meta metadata.Response) {
-	c.topicPartitionLeadersMux.Lock()
-	defer c.topicPartitionLeadersMux.Unlock()
+	wg.Wait()
 }
